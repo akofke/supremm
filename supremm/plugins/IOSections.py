d4d6403d80fe099799c23e99a9e26405a0d0c705
Alex Kofke <adkofke@buffalo.edu>
2017-05-24 17:08:23 -0400

    IOSections report read and write bytes
    Also fix errors caused by incorrect timestamp conversion

--- supremm/plugins/IOSections.py
+++ supremm/plugins/IOSections.py
@@ -6,6 +6,7 @@ import time
 import numpy
 
 from supremm.plugin import Plugin
+from supremm.errors import ProcessingError
 
 
 class IOSections(Plugin):
@@ -19,34 +20,45 @@ class IOSections(Plugin):
 
     def __init__(self, job):
         super(IOSections, self).__init__(job)
-        self.starttime = time.mktime(job.start_datetime.timetuple())
-        self.endtime = time.mktime(job.end_datetime.timetuple())
+        self.starttime = (job.start_datetime - datetime.datetime(1970, 1, 1)).total_seconds()
+        self.endtime = (job.end_datetime - datetime.datetime(1970, 1, 1)).total_seconds()
         self.quarter = (self.endtime - self.starttime) / 4
         self.current_marker = self.starttime + self.quarter
 
-        self.section_start_data = 0
+        self.section_start_data = None
         self.section_start_timestamp = self.starttime
         self.quarter_avgs = []
-        self.last_value = 0
+        self.last_value = []
 
 
     def process(self, nodemeta, timestamp, data, description):
-        mountpoint_sum = numpy.sum(data[0])
-        self.last_value = mountpoint_sum
+        mountpoint_sums = [numpy.sum(x) for x in data]
+        self.last_value = mountpoint_sums
+        if self.section_start_data is None:
+            self.section_start_data = mountpoint_sums
+
 
         if timestamp >= self.current_marker:
-            avg = (mountpoint_sum - self.section_start_data) / (timestamp - self.section_start_timestamp)
-            self.quarter_avgs.append(avg)
+            avg_read = (mountpoint_sums[0] - self.section_start_data[0]) / (timestamp - self.section_start_timestamp)
+            avg_write = (mountpoint_sums[1] - self.section_start_data[1]) / (timestamp - self.section_start_timestamp)
+
+            self.quarter_avgs.append((avg_read, avg_write))
             self.current_marker += self.quarter
-            self.section_start_data = mountpoint_sum
+            self.section_start_data = mountpoint_sums
             self.section_start_timestamp = timestamp
 
         return True
 
     def results(self):
-        self.quarter_avgs.append((self.last_value - self.section_start_data) / (self.endtime - self.section_start_timestamp))
+        avg_read = (self.last_value[0] - self.section_start_data[0]) / (self.endtime - self.section_start_timestamp)
+        avg_write = (self.last_value[1] - self.section_start_data[1]) / (self.endtime - self.section_start_timestamp)
+        
+        self.quarter_avgs.append((avg_read, avg_write))
+        if len(self.quarter_avgs) != 4:
+            return {"error": ProcessingError.INSUFFICIENT_DATA}
         return { 
-                "first_quarter": self.quarter_avgs[0],
-                "middle_half": (self.quarter_avgs[1] + self.quarter_avgs[2]) / 2,
-                "last_quarter": self.quarter_avgs[3]
+                "1": self.quarter_avgs[0],
+                "2": self.quarter_avgs[1],
+                "3": self.quarter_avgs[2],
+                "4": self.quarter_avgs[3]
                 }

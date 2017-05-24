e05620512cb65218851f2c7f289d181ea2e3c382
Alex Kofke <adkofke@buffalo.edu>
2017-05-24 14:59:00 -0400

    Add implementation for IO sections plugin
    Finds average IO rate for first quarter, middle half,
    and last quarter of job.

--- supremm/plugins/IOSections.py
+++ supremm/plugins/IOSections.py
@@ -1,27 +1,52 @@
 #!/usr/bin/env python
-
 from __future__ import print_function
+
+import datetime
+import time
+import numpy
+
 from supremm.plugin import Plugin
 
+
 class IOSections(Plugin):
-    
+
     name = property(lambda x: "iosections")
     mode = property(lambda x: "all")
-    requiredMetrics = property(lambda x: ["gpfs.fsios.read_bytes", "gpfs.fsios.write_bytes"]) 
-  
+    requiredMetrics = property(lambda x: ["gpfs.fsios.read_bytes", "gpfs.fsios.write_bytes"])
+
     optionalMetrics = property(lambda x: [])
     derivedMetrics = property(lambda x: [])
 
     def __init__(self, job):
         super(IOSections, self).__init__(job)
-        self.starttime = job.start_datetime
-        self.endtime = job.end_datetime
-        print("hello")
-                
+        self.starttime = time.mktime(job.start_datetime.timetuple())
+        self.endtime = time.mktime(job.end_datetime.timetuple())
+        self.quarter = (self.endtime - self.starttime) / 4
+        self.current_marker = self.starttime + self.quarter
+
+        self.section_start_data = 0
+        self.section_start_timestamp = self.starttime
+        self.quarter_avgs = []
+        self.last_value = 0
+
+
     def process(self, nodemeta, timestamp, data, description):
-        print(timestamp)
-        print(data)
+        mountpoint_sum = numpy.sum(data[0])
+        self.last_value = mountpoint_sum
+
+        if timestamp >= self.current_marker:
+            avg = (mountpoint_sum - self.section_start_data) / (timestamp - self.section_start_timestamp)
+            self.quarter_avgs.append(avg)
+            self.current_marker += self.quarter
+            self.section_start_data = mountpoint_sum
+            self.section_start_timestamp = timestamp
+
+        return True
 
     def results(self):
-        return { "test": "test" }
- 
+        self.quarter_avgs.append((self.last_value - self.section_start_data) / (self.endtime - self.section_start_timestamp))
+        return { 
+                "first_quarter": self.quarter_avgs[0],
+                "middle_half": (self.quarter_avgs[1] + self.quarter_avgs[2]) / 2,
+                "last_quarter": self.quarter_avgs[3]
+                }

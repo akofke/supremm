beb32bcbf7df17d112573232d57919aa536f3254
Alex Kofke <adkofke@buffalo.edu>
2017-05-25 14:15:33 -0400

    IOSections plugin add support for multiple nodes
    Use a dictionary to track data for each node independently.
    Should be able to handle receiving interleaved node data points.

--- supremm/plugins/IOSections.py
+++ supremm/plugins/IOSections.py
@@ -23,42 +23,58 @@ class IOSections(Plugin):
         self.starttime = (job.start_datetime - datetime.datetime(1970, 1, 1)).total_seconds()
         self.endtime = (job.end_datetime - datetime.datetime(1970, 1, 1)).total_seconds()
         self.quarter = (self.endtime - self.starttime) / 4
-        self.current_marker = self.starttime + self.quarter
 
-        self.section_start_data = None
-        self.section_start_timestamp = self.starttime
-        self.quarter_avgs = []
-        self.last_value = []
+        self.nodes = {}
 
 
     def process(self, nodemeta, timestamp, data, description):
+        n = nodemeta.nodename
+        if n not in self.nodes:
+            self.nodes[n] = {
+                        "current_marker": self.starttime + self.quarter,
+                        "section_start_data": None,
+                        "section_start_timestamp": self.starttime,
+                        "quarter_avgs": [],
+                        "last_value": []
+                    }
+
+        node_data = self.nodes[n]
+
         mountpoint_sums = [numpy.sum(x) for x in data]
-        self.last_value = mountpoint_sums
-        if self.section_start_data is None:
-            self.section_start_data = mountpoint_sums
+        node_data["last_value"] = mountpoint_sums
+        if node_data["section_start_data"] is None:
+            node_data["section_start_data"] = mountpoint_sums
 
 
-        if timestamp >= self.current_marker:
-            avg_read = (mountpoint_sums[0] - self.section_start_data[0]) / (timestamp - self.section_start_timestamp)
-            avg_write = (mountpoint_sums[1] - self.section_start_data[1]) / (timestamp - self.section_start_timestamp)
+        if timestamp >= node_data["current_marker"]:
+            avg_read = (mountpoint_sums[0] - node_data["section_start_data"][0]) / (timestamp - node_data["section_start_timestamp"])
+            avg_write = (mountpoint_sums[1] - node_data["section_start_data"][1]) / (timestamp - node_data["section_start_timestamp"])
 
-            self.quarter_avgs.append((avg_read, avg_write))
-            self.current_marker += self.quarter
-            self.section_start_data = mountpoint_sums
-            self.section_start_timestamp = timestamp
+            node_data["quarter_avgs"].append((avg_read, avg_write))
+            node_data["current_marker"] += self.quarter
+            node_data["section_start_data"] = mountpoint_sums
+            node_data["section_start_timestamp"] = timestamp
 
         return True
 
     def results(self):
-        avg_read = (self.last_value[0] - self.section_start_data[0]) / (self.endtime - self.section_start_timestamp)
-        avg_write = (self.last_value[1] - self.section_start_data[1]) / (self.endtime - self.section_start_timestamp)
-        
-        self.quarter_avgs.append((avg_read, avg_write))
-        if len(self.quarter_avgs) != 4:
-            return {"error": ProcessingError.INSUFFICIENT_DATA}
-        return { 
-                "1": self.quarter_avgs[0],
-                "2": self.quarter_avgs[1],
-                "3": self.quarter_avgs[2],
-                "4": self.quarter_avgs[3]
-                }
+
+        for node, data in self.nodes.iteritems():
+            avg_read = (data["last_value"][0] - data["section_start_data"][0]) / (self.endtime - data["section_start_timestamp"])
+            avg_write = (data["last_value"][1] - data["section_start_data"][1]) / (self.endtime - data["section_start_timestamp"])
+
+            data["quarter_avgs"].append((avg_read, avg_write))
+
+            if len(data["quarter_avgs"]) != 4:
+                return {"error": ProcessingError.INSUFFICIENT_DATA}
+
+            data = {k: v for k, v in data.iteritems() if k == "quarter_avgs"}
+            self.nodes[node] = data
+
+        return self.nodes
+    # {
+    #             "1": self.quarter_avgs[0],
+    #             "2": self.quarter_avgs[1],
+    #             "3": self.quarter_avgs[2],
+    #             "4": self.quarter_avgs[3]
+    #             }
